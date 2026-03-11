31e90b4be4c713aadb5e96202612f6d3781deed9
gustavo linhares <gals2@cin.ufpe.br>
2026-03-11 19:07:15 -0300

    Create piramide.c

---
+++ 0x02/piramide.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+
+        char c;
+        scanf("%c", &c);
+
+        for (char now = 'A'; now <= c; now++) {
+
+                for (char dot = 0; dot < (c - now); dot++) {
+                        printf(".");
+                }
+
+                char init = 'A';
+
+                for (char t = 'A'; t < now; t++) {
+                        printf("%c", t);
+                }
+
+                for (int j = 0; now >= init; j++) {
+                        init++;
+                        printf("%c", now - j);
+                }
+
+                for (char dot = 0; dot < (c - now); dot++) {
+                        printf(".");
+                }
+
+                printf("\n");
+
+        }
+
+}
