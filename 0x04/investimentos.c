cd757ada584c7452612f9561643ad8f48d723ffe
gustavo linhares <gals2@cin.ufpe.br>
2026-04-04 16:13:21 -0300

    Update investimentos.c

--- 0x04/investimentos.c
+++ 0x04/investimentos.c
@@ -2,30 +2,31 @@
 
 int main(void) {
 
-        char movs[42];
+        char movs[100];
         int matrix[4][4] = {{0, 0}};
+        // variaveis de controle comuns na hora de encontrar o maior/menor elemento de uma matriz -> current + greater
         int curr_line = 0;
         int curr_column = 0;
         int greater = 0;
         int x_greater = 0;
         int y_greater = 0;
 
-        fgets(movs, 42, stdin);
+        fgets(movs, 100, stdin);
 
         for (int k = 0; k < 42; k++) {
 
-            if (movs[k] != ' ') {
+            if (movs[k] != ' ') { // a sequencia de movimento contém espaços, mas eles nao me interessam
 
-                if (movs[k] == 'b') {
+                if (movs[k] == 'b') { // baixo: aumenta a linha atual e incrementa em 1 o novo indice correspondente
                     curr_line++;
                     matrix[curr_line][curr_column]++;
-                } else if (movs[k] == 'c') {
+                } else if (movs[k] == 'c') { // cima: diminui a linha atual e incrementa em 1 o novo indice correspondente
                     curr_line--;
                     matrix[curr_line][curr_column]++;
-                } else if (movs[k] == 'd') {
+                } else if (movs[k] == 'd') { // direita: aumenta a coluna atual e incrementa em 1 o novo indice correspondente
                     curr_column++;
                     matrix[curr_line][curr_column]++;
-                } else if (movs[k] == 'e') {
+                } else if (movs[k] == 'e') { // esquerda: diminui a coluna atual e incrementa em 1 o novo indice correspondente
                     curr_column--;
                     matrix[curr_line][curr_column]++;
                 }
@@ -33,12 +34,12 @@ int main(void) {
             }
         }
 
+        // método clássico de encontrar o maior elemento em uma matriz
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-
                 if (i == 0 && j == 0) {
-                    greater = matrix[i][j];
-                } else if (matrix[i][j] > greater) {
+                    greater = matrix[i][j]; // definimos o primeiro elemento como o maior
+                } else if (matrix[i][j] > greater) { // continua lendo a matriz, se encontrar um valor maior, ele passa a ser o novo maaior (confuso, mas vc entendeu)
                     greater = matrix[i][j];
                     x_greater = i;
                     y_greater = j;
