e9fc07059207547bead938ed5d4f59bbe3c8a9b2
gustavo linhares <gals2@cin.ufpe.br>
2026-04-04 16:26:48 -0300

    Update enfeite.c

--- 0x04/enfeite.c
+++ 0x04/enfeite.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
+#include <string.h> // strcpy()
 
-int main(void)
-
+int main(void) {
+    
     int m, n;
+    
     scanf("%d %d", &m, &n);
-
     char plaza[m][n + 1];
     char result[m][n + 1];
 
+    // essa trick pode ser utilizada em basicmente em todas as questões
+    // consiste em iterar sobre a matriz original e fazer as alterações em uma matriz auxiliar
     for (int i = 0; i < m; i++) {
         scanf("%s", plaza[i]);
-        for (int j = 0; j < n; j++) {
-            result[i][j] = plaza[i][j];
-        }
-        result[i][j] = '\0';
+        strcpy(result[i], plaza[i]);
     }
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-
+            // itera sobre a matriz e acha um poste
             if (plaza[i][j] == 'P') {
-
+                // como precisaamos verificar as adjacencias do poste, precisamos verificaar o que está acima, abaixo, à direita e à esquerda
+                // para não fazer um condicional para cada caso, criamos um for de -1 (cima/esquerda) a +1 (baixo, direita) pra linha e coluna
                 for (int d_line = -1; d_line <= 1; d_line++) {
                     for (int d_column = -1; d_column <= 1; d_column++) {
+                        
+                        if (d_line == 0 && d_column == 0) {
+                            continue; // pula o próprio poste
+                        }
 
-                        int line_adjc = i + d_line;
-                        int column_adjc = j + d_column;
-
-                        if (line_adjc >= 0 && line_adjc < m && column_adjc >= 0 && column_adjc < n) {
-
-                            if (plaza[line_adjc][column_adjc] != 'P') {
-
-                                if (d_line == 0 || d_column == 0) {
-                                    if (d_line != 0 || d_column != 0) { // não é o próprio centro
-                                        result[line_adjc][column_adjc] = '+';
-                                    }
-                                } else {
-                                    if (result[line_adjc][column_adjc] == '-') {
-                                        result[line_adjc][column_adjc] = 'x';
-                                    }
-                                }
+                        int ni = i + d_line; // cima/baixo
+                        int nj = j + d_column; // direita/esquerda
+                        // verifica os limites da matriz e vê se não é poste
+                        if (ni >= 0 && ni < m && nj >= 0 && nj < n && result[ni][nj] != 'P') {
+                            
+                            if (d_line == 0 || d_column == 0) { // mesma linha ou mesma coluna do poste = cima/baixo + direita/esquerda -> recebe o +
+                                result[ni][nj] = '+';
+                            }
+                            else if (result[ni][nj] == '-') { // se não recebe o + e tá livre, recebe o x
+                                result[ni][nj] = 'x';
                             }
                         }
                     }
@@ -47,9 +46,9 @@ int main(void)
             }
         }
     }
-
-    for (int l = 0; l < m; l++) {
-        printf("%s\n", result[l]);
+    // mostra o resultado
+    for (int i = 0; i < m; i++) {
+        printf("%s\n", result[i]);
     }
 
     return 0;
