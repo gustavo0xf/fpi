2df992d3133c080119a5f0094bf969abe317f19a
gustavo linhares <gals2@cin.ufpe.br>
2026-04-04 16:07:11 -0300

    Update goteira.c

--- 0x04/goteira.c
+++ 0x04/goteira.c
@@ -8,39 +8,37 @@ int main(void) {
     char room[m][n + 1];
     
     for (int i = 0; i < m; i++) {
-        scanf("%s", &room[i]); 
+        scanf("%s", &room[i]); // montagem da matriz de chars
     }
     
-    int water = 1;
+    int water = 1; // flag de controle, verifica se a água ainda pode cair
 
     while (water) {
         
-        water = 0; // flag de controle, verifica se a água ainda pode cair
-        
+        water = 0; // evitar que o loop seja infinito
         // varrendo a matriz do quarto
         for (int i = 0; i < m - 1; i++) {
             for (int j = 0; j < n; j++) {
                 
-                // ao encontrar uma gota d'agua, começar a declarar as condições p/ ela cair
+                // varre a matriz e acha a gotinha
                 if (room[i][j] == 'o') {
                     
-                    // se não tiver nenhuma prateleira, molha 
+                    // se não tiver nenhuma prateleira  abaixo, molha 
                     if (room[i + 1][j] == '.') {
                         room[i + 1][j] = 'o';
-                        water = 1;
+                        water = 1; // pode continuar caindo
                     }
-                    
-                    // bateu na prateleira, espalha pros lados
+                    // se tiver uma prateleira abaixo, escorre pros lados
                     else if (room[i + 1][j] == '#') {
                         // espalha pra esquerda
-                        if (j > 0 && room[i][j - 1] == '.') { // j = 0 -> extrema esquerda
-                            room[i][j - 1] = 'o'; // muda a gotinha pra esquerda pra continuar a descida
-                            water = 1;
+                        if (j > 0 && room[i][j - 1] == '.') { // verifica os limiites da matriz e verifica se tá livre pra molhar
+                            room[i][j - 1] = 'o';
+                            water = 1; // pode continuar caindo
                         }
                         // espalha pra direita
-                        if (j < n - 1 && room[i][j + 1] == '.') { // j = n - 1 -> extrema direita
+                        if (j < n - 1 && room[i][j + 1] == '.') { // verifica os limites da matriz e verifica se tá livre pra molhar
                             room[i][j + 1] = 'o'; // muda a gotinha pra direita pra continuar a descida
-                            water = 1;
+                            water = 1; // pode continuar caindo
                         }
                     }
                 }
@@ -48,7 +46,7 @@ int main(void) {
         }
     }
     
-    // quarto molhado
+    // retorna o quarto molhado
     for (int i = 0; i < m; i++) {
         printf("%s\n", room[i]);
     }
