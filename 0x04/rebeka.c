7eebdf08ece550b69a6c43d75b56fafb1889d509
gustavo linhares <gals2@cin.ufpe.br>
2026-04-04 16:51:12 -0300

    Update rebeka.c

--- 0x04/rebeka.c
+++ 0x04/rebeka.c
@@ -1,66 +1,70 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(void) {
     
     int m, n;
-    int distance = 0;
-    int curr_line;
-    int curr_column;
-    bool win = false;
-
+    
     scanf("%dx%d", &m, &n);
     char map[m][n + 1];
-    
+    int dist[m][n]; // matriz pr reegistrar a distancia que cada elemento está da origem
+    // variaveis de controle para marcar a posição do destino
+    int x_goal = -1;
+    int y_goal = -1;
+
     for (int i = 0; i < m; i++) {
-        scanf("%s", &map[i]);
-    }
-    
-    for (int i = 0; i < m; i++) {
+        scanf("%s", map[i]); // monta o mapa
         for (int j = 0; j < n; j++) {
             
+            dist[i][j] = 9999; // ao mesmo tempo, preenche a matriz de distancias com 9999 em tudo que não for a origem
+            
             if (map[i][j] == 'o') {
-                
-                curr_line = i;
-                curr_column = j;
-                
-                if (map[i + 1][j] == '_' && map[i + 1][j] != 'd') {
-                    curr_line++;
-                    distance++;
-                    map[i][j] = 'x';
-                    map[curr_line][curr_column] = 'o';
-                } else if (map[i - 1][j] == '_' && map[i - 1][j] != 'd') {
-                    curr_line--;
-                    distance++;
-                    map[i][j] = 'x';
-                    map[curr_line][curr_column] = 'o';
-                } else if (map[i][j + 1] == '_' && map[i][j + 1] != 'd') {
-                    curr_column++;
-                    distance++;
-                    map[i][j] = 'x';
-                    map[curr_line][curr_column] = 'o';
-                } else if (map[i][j - 1] == '_' && map[i][j - 1] != 'd') {
-                    curr_column--;
-                    distance++;
-                    map[i][j] = 'x';
-                    map[curr_line][curr_column] = 'o';
-                }
-                
-                if (map[i + 1][j] == 'd' || map[i - 1][j] == 'd' || map[i][j + 1] == 'd' || map[i][j - 1] == 'd') {
-                    distance++;
-                    win = true;
-                }
-                
+                dist[i][j] = 0; // a distancia da origem pra origem eh 0
+            }
+            
+            if (map[i][j] == 'd') { // registra a posição de destino
+                x_goal = i; 
+                y_goal = j; 
             }
             
         }
     }
-    
-    if (win) {
-        printf("Apos correr %d metros e quase desistir por causa da dist�ncia, Rebeka conseguiu escapar!\n", distance);
-    } else {
-        printf("Poxa... Parece que nao foi dessa vez que Rebeka conseguiu fugir\n");   
+
+    for (int step = 0; step < m * n; step++) { // simular a caminhada da rebeka
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                if (dist[i][j] == step) {
+                    // mesma trick da questão da praça, agora com a feature de não andar em diagonal
+                    for (int d_line = -1; d_line <= 1; d_line++) {
+                        for (int d_col = -1; d_col <= 1; d_col++) {
+                            
+                            if ((d_line == 0 || d_col == 0) && (d_line != d_col)) { // delta line e delta column iguais implicam em uma mov. diagonal
+                                
+                                int ni = i + d_line;
+                                int nj = j + d_col;
+
+                                if (ni >= 0 && ni < m && nj >= 0 && nj < n) { // verifica os limites da matriz
+                                    if (map[ni][nj] != '#' && dist[ni][nj] > step + 1) { // verific se não tem obstaculos
+                                        dist[ni][nj] = step + 1; // atualiza a matriz de distancias
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (dist[x_goal][y_goal] != 9999) { // se a matriz de distancias, no indice do destino, for diferente de 9999, é pq foi alterada, então, é pq rebeka chegou
+            break;
+        }
     }
-    
+
+    // mensagens finais
+    if (dist[x_goal][y_goal] != 9999) {
+        printf("Apos correr %d metros e quase desistir por causa da dist�ncia, Rebeka conseguiu escapar!\n", dist[x_goal][y_goal]);
+    } else {
+        printf("Poxa... Parece que nao foi dessa vez que Rebeka conseguiu fugir\n");
+    }
+
     return 0;
 }
