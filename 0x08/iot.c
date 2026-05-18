a10c90a5240752407cb0f498d03920a785aae02a
gustavo linhares <gals2@cin.ufpe.br>
2026-05-18 11:41:16 -0300

    Update iot.c

--- 0x08/iot.c
+++ 0x08/iot.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 // minha macro default
 #define FORi(n) for (int i = 0; i < n; i++)
-// union com o byte bruto e a struct com os bitfields
+// typedef para o tipo unsigned char e para a union com o byte bruto e a struct com os bitfields
+typedef unsigned char uchar
 typedef union {
-        unsigned char rawByte;  // fail
+        uchar rawByte;          // byte bruto
         struct {
-                unsigned char err  : 1; // 1 bit (LSB)
-                unsigned char mode : 3; // 3 bits
-                unsigned char read : 4; // 4 bits (MSB)
+                uchar err  : 1; // 1 bit (LSB)
+                uchar mode : 3; // 3 bits
+                uchar read : 4; // 4 bits (MSB)
         } fields;
 } SensorStatus;
 // a funcao recebe o valor de teste e acontece o truncamento para preencher os bitfields, considerando a ordem definida no enunciado
-void manageSensor(unsigned char value) {
+void manageSensor(uchar value) {
         SensorStatus status;
         status.rawByte = value;
         printf("Byte bruto: %u | Erro: %u | Modo: %u | Leitura: %u\n", status.rawByte, status.fields.err, status.fields.mode,status.fields.read);
 }
 // efetuando os testes
 int main(void) {
-    unsigned char tests[] = {0, 255, 162, 85, 200};
+    uchar tests[] = {0, 255, 162, 85, 200};
 
     FORi(5) {
         manageSensor(tests[i]);
