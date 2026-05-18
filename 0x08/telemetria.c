c2322895da8f0215e87d951b20efde87da1bf252
gustavo linhares <gals2@cin.ufpe.br>
2026-05-18 12:13:42 -0300

    Update telemetria.c

--- 0x08/telemetria.c
+++ 0x08/telemetria.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+// macros
 #define FORi(n) for (int i = 0; i < n; i++)
+// typedefs para o tipo uint32_t e para a union do pacote de telemetria
 typedef uint32_t u32;
-typedef int32_t  i32;
-
 typedef union {
     u32 raw;
     struct {
         u32 id      : 10;
         u32 status  : 3;
         u32 battery : 1;
-        i32 temp    : 12;
+        u32 temp    : 12;
         u32 parity  : 6;
     } fields;
 } TelPacket;
-
+// funcao para alocar memoria dinamicamente para o ponteiro para a union
 TelPacket* allocMem(int cap) {
     TelPacket *p = (TelPacket *) malloc(cap * sizeof(TelPacket));
     if (p == NULL) {
@@ -25,9 +24,9 @@ TelPacket* allocMem(int cap) {
     }
     return p;
 }
-
+// funcao para montar os pacotes
 TelPacket* buildPacket(TelPacket *packet, int *total, int *cap, u32 raw) {
-    if (*total == *cap) {
+    if (*total == *cap) { // capacidade maxima atingida -> dobrar o tamanho com a realloc
         *cap *= 2;
         TelPacket *tmp = (TelPacket *) realloc(packet, (*cap) * sizeof(TelPacket));
         if (tmp == NULL) {
@@ -41,24 +40,24 @@ TelPacket* buildPacket(TelPacket *packet, int *total, int *cap, u32 raw) {
     (*total)++;
     return packet;
 }
-
+// relatorio final
 void printReport(TelPacket *packet, int total) {
     FORi(total) {
         printf("Pacote [%d] - Dado Bruto: 0x%08X\n", i + 1, packet[i].raw);
         printf("ID do Sensor : %u\n", packet[i].fields.id);
         printf("Status : %u\n", packet[i].fields.status);
         printf("Bateria Baixa: %s\n", packet[i].fields.battery ? "SIM (ALERTA)" : "Nao");
-        printf("Temperatura : %d graus\n", packet[i].fields.temp);
+        printf("Temperatura : %d graus\n", ~packet[i].fields.temp + 1);
         printf("-------------------------------------------------\n");
     }
 }
-
+// liberar a memória que fora alocada
 void freeMem(TelPacket *packet) {
     if (packet != NULL) {
         free(packet);
     }
 }
-
+// chamando todas as funções
 int main(void) {
     int cap = 2;
     int total = 0;
